f50b25c0e0590b61fade2d15ad99ad309299a842
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-11-14 07:26:10 +0700

    update validation booking

--- resources/views/emails/validation/daily.blade.php
+++ resources/views/emails/validation/daily.blade.php
@@ -1,11 +1,11 @@
 @component('mail::message')
-<img src="https://compro.justnear.my.id/icon/checked.png" alt="Circle Check" class="img-fluid" style="display:block;margin:auto;margin-bottom:20px;text-align:center;width:50px;">
+<img src="{{ asset('icon/checked.png') }}" alt="Circle Check" class="img-fluid" style="display:block;margin:auto;margin-bottom:20px;text-align:center;width:50px;">
 <h1 class="text-center mb-4">Validasi Berhsasil</h1>
 <div class="row mb-2"><div class="col-12">Halo {{ $data->first_name }},</div></div>
 <div class="row mb-2"><div class="col-12">Kami senang memberitahu Anda bahwa proses validasi Anda baru-baru ini berhasil. Berikut adalah kode PIN Anda:</div></div>
 <h1>{{ $data->pin }}</h1>
 <div class="row mb-2"><div class="col-12">Harap simpan PIN ini dengan kerahasiaan dan gunakan untuk proses masuk Anda ke dalam ruangan.</div></div>
-<div class="row mb-2"><div class="col-12">Harap diingat bahwa kode PIN ini akan berlaku hingga <b>{{ $data->expired }}</b>. Setelah waktu tersebut, kode ini akan tidak berlaku dan Anda perlu melakukan proses booking kembali.</div></div>
+<div class="row mb-2"><div class="col-12">Harap diingat bahwa kode PIN ini akan berlaku hingga <b>{{ date('d-m-Y H:i:s', strtotime($data->expired)) }}</b>. Setelah waktu tersebut, kode ini akan tidak berlaku dan Anda perlu melakukan proses booking kembali.</div></div>
 <div class="row mb-2"><div class="col-12">Selain itu, kami telah membuatkan kode QR untuk kenyamanan Anda:</div></div>
 <div class="row mb-2"><div class="col-12">{!! $data->qr !!}</div></div>
 <div class="row mb-2"><div class="col-12">Jika Anda memiliki pertanyaan atau kekhawatiran, jangan ragu untuk menghubungi tim dukungan kami.</div></div>
