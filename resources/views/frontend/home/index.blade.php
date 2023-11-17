8e0316be21a325ecb1da6e2983961298dcd527bc
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-11-17 15:52:35 +0700

    update homw

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -115,6 +115,55 @@
             </div>
         </section><!-- End Counts Section -->
 
+        <!-- ======= Features Section ======= -->
+        <section id="features" class="features">
+            <div class="container" data-aos="fade-up">
+                <header class="section-header">
+                    <h2>Fasilitas</h2>
+                    <p>Beberapa fasilitas yang tersedia di kami.</p>
+                </header>
+
+                <div class="row">
+                    <div class="col-lg-6">
+                        <img src="{{ asset('frontend') }}/assets/img/values-3.png" class="img-fluid" alt="">
+                    </div>
+
+                    <div class="col-lg-6 mt-5 mt-lg-0 d-flex">
+                        <div class="row align-self-center gy-4">
+
+                            <div class="col-md-6" data-aos="zoom-out" data-aos-delay="200">
+                            <div class="feature-box d-flex align-items-center">
+                                <i class="bi bi-check"></i>
+                                <h3>Whirlpool & Steam</h3>
+                            </div>
+                            </div>
+
+                            <div class="col-md-6" data-aos="zoom-out" data-aos-delay="300">
+                            <div class="feature-box d-flex align-items-center">
+                                <i class="bi bi-check"></i>
+                                <h3>Auditorium & Function Room</h3>
+                            </div>
+                            </div>
+
+                            <div class="col-md-6" data-aos="zoom-out" data-aos-delay="400">
+                            <div class="feature-box d-flex align-items-center">
+                                <i class="bi bi-check"></i>
+                                <h3>Pool Side Café & Kantin</h3>
+                            </div>
+                            </div>
+
+                            <div class="col-md-6" data-aos="zoom-out" data-aos-delay="500">
+                            <div class="feature-box d-flex align-items-center">
+                                <i class="bi bi-check"></i>
+                                <h3>Aerobik</h3>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> <!-- / row -->
+            </div>
+        </section>
+
         <!-- ======= Pricing Section ======= -->
         <section id="booking" class="pricing">
 
