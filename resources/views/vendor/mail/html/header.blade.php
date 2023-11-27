8768aabc2d3af9870f6d27a89c017fc48cde021e
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-11-27 10:18:16 +0700

    update logo in body email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,8 +5,7 @@
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
-{{-- <img src="{{ asset('frontend') }}/assets/img/Logo-SSRC-cut.webp" class="logo" alt="Logo"> --}}
-{{-- {{ $slot }} --}}
+<img src="frontend/assets/img/Logo-SSRC-cut.webp" class="logo" alt="Logo">
 @endif
 </a>
 </td>
