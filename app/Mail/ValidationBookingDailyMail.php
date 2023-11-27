9525548445f88c587a250a40b38ee4c40d1a8c43
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-11-27 14:11:11 +0700

    update to png in qr

--- app/Mail/ValidationBookingDailyMail.php
+++ app/Mail/ValidationBookingDailyMail.php
@@ -28,22 +28,12 @@ class ValidationBookingDailyMail extends Mailable
     public function build()
     {
         $qrPath = public_path('qr_codes/' . $this->data->qr);
-        $pdfFileName = 'qr_' . time() . '.pdf';
-
-        // Generate QR code PDF with white background
-        QrCode::size(300)
-            ->format('pdf')
-            ->backgroundColor(255, 255, 255) // Putih
-            ->margin(20)
-            ->generate($this->data->qr, $qrPath . '/' . $pdfFileName);
-
-        // Attach the PDF to the email
-        $pdfContent = Storage::get('qr_codes/' . $pdfFileName);
 
         return $this->markdown('emails.validation.daily')
                     ->subject('Validasi Booking Harian')
-                    ->attachData($pdfContent, $pdfFileName, [
-                        'mime' => 'application/pdf',
+                    ->attach($qrPath, [
+                        'as' => 'QR_Code.png',
+                        'mime' => 'image/png',
                     ]);
     }
 }
