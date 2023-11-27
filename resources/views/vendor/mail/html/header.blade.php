21986e4ddece70af77d732178ff417e0ff46ebca
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-11-27 14:14:05 +0700

    update header email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,8 +4,8 @@
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-<img src="frontend/assets/img/Logo-SSRC-cut.png" style="display:block;margin:auto;margin-bottom:20px;text-align:center;width:50px;" alt="Logo">
+{{-- @else
+<img src="frontend/assets/img/Logo-SSRC-cut.png" style="display:block;margin:auto;margin-bottom:20px;text-align:center;width:50px;" alt="Logo"> --}}
 @endif
 </a>
 </td>
