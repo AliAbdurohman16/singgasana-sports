68497e773733e0e78aa218d6546c33486adaa28e
AliAbdurohman16 <alyabdurohman16@gmail.com>
2024-02-10 14:28:47 +0700

    adding dashboar, pages and articles

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Dashboard\DashboardController;
 use App\Http\Controllers\Api\Booking\MemberController;
 use App\Http\Controllers\Api\Service\ServiceController;
+use App\Http\Controllers\Api\Page\PageController;
+use App\Http\Controllers\Api\Article\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,8 +27,17 @@ Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
 
+    // Dashboard
+    Route::get('dashboard', [DashboardController::class, 'index']);
+
     // Services
     Route::get('services', [ServiceController::class, 'index']);
+
+    // Pages
+    Route::get('pages/{slug}', [PageController::class, 'index']);
+
+    // Article
+    Route::get('articles/{slug}', [ArticleController::class, 'index']);
 
     // Booking
     Route::name('booking.')->prefix('booking')->group(function () {
