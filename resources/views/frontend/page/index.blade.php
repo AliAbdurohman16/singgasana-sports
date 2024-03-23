42392ae4bac20f7f7541c22d70c77f27a3d7d163
AliAbdurohman16 <alyabdurohman16@gmail.com>
2024-03-23 12:06:42 +0700

    revision

--- resources/views/frontend/page/index.blade.php
+++ resources/views/frontend/page/index.blade.php
@@ -23,27 +23,36 @@
         @foreach ($pages as $row)
         <div class="row gy-4">
 
-        @if ($row->images->count() > 0)
-            <div class="col-lg-6">
-                <div class="facility-details-slider swiper">
-                    <div class="swiper-wrapper align-items-center">
-                        @foreach ($row->images as $image)
-                            <div class="swiper-slide">
-                                <img src="{{ asset('storage/page/'.$image->path) }}" alt="image-pages">
-                            </div>
-                        @endforeach
+            @if ($row->images->count() > 0)
+                <div class="col-lg-6">
+                    <div class="facility-details-slider swiper">
+                        <div class="swiper-wrapper align-items-center">
+                            @foreach ($row->images as $image)
+                                <div class="swiper-slide">
+                                    <img src="{{ asset('storage/page/'.$image->path) }}" alt="image-pages">
+                                </div>
+                            @endforeach
+                        </div>
+                        <div class="swiper-pagination"></div>
                     </div>
-                    <div class="swiper-pagination"></div>
                 </div>
-            </div>
-        @endif
+            @endif
 
-          <div class="{{ route('pages.index', 'privacy-policy') ? 'col-lg-12' : 'col-lg-6' }}">
-            <div class="facility-description">
-              <h2 class="{{ route('pages.index', 'privacy-policy') ? 'text-center' : '' }}">{{ $row->title }}</h2>
-              <p>{!! $row->content !!}</p>
-            </div>
-          </div>
+            @if (!request()->is('pages/privacy-policy') && !request()->is('pages/privacy-policy-for-app'))
+                <div class="col-lg-6">
+                    <div class="facility-description">
+                        <h2>{{ $row->title }}</h2>
+                        <p>{!! $row->content !!}</p>
+                    </div>
+                </div>
+            @else
+                <div class="col-lg-12">
+                    <div class="facility-description">
+                        <h2 class="text-center">{{ $row->title }}</h2>
+                        <p>{!! $row->content !!}</p>
+                    </div>
+                </div>
+            @endif
 
         </div>
         @endforeach
