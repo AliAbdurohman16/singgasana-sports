dcab1ee792eae4ffa537a6e64b1ca63edd05157a
AliAbdurohman16 <alyabdurohman16@gmail.com>
2024-08-11 15:56:33 +0700

    hide icon

--- resources/views/emails/validation/school.blade.php
+++ resources/views/emails/validation/school.blade.php
@@ -1,5 +1,5 @@
 @component('mail::message')
-<img src="{{ asset('icon/checked.png') }}" alt="Circle Check" class="img-fluid" style="display:block;margin:auto;margin-bottom:20px;text-align:center;width:50px;">
+<!-- <img src="{{ asset('icon/checked.png') }}" alt="Circle Check" class="img-fluid" style="display:block;margin:auto;margin-bottom:20px;text-align:center;width:50px;"> -->
 <h1 class="text-center mb-4">Validasi Berhasil</h1>
 <div class="row mb-2"><div class="col-12">Halo {{ $data->user->first_name }},</div></div>
 <div class="row mb-2"><div class="col-12">Kami senang memberitahu Anda bahwa proses validasi Anda baru-baru ini berhasil.</div></div>
