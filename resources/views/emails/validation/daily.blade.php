f959fb2f1552172394f2d5606d8553f3ce9e0a9d
AliAbdurohman16 <alyabdurohman16@gmail.com>
2024-08-11 21:09:31 +0700

    test button unduh

--- resources/views/emails/validation/daily.blade.php
+++ resources/views/emails/validation/daily.blade.php
@@ -9,9 +9,12 @@
         <img src="{{ asset('singgasana-sport/public/qr_codes/' . $data->qr) }}" style="display: block; margin: auto; text-align: center; width: 200px;" alt="QR Code">
     </div>
 </div>
-@component('mail::button', ['url' => asset('singgasana-sport/public/qr_codes/' . $data->qr), 'color' => 'primary'])
+<!-- @component('mail::button', ['url' => asset('singgasana-sport/public/qr_codes/' . $data->qr), 'color' => 'primary'])
 Unduh QR Code
-@endcomponent
+@endcomponent -->
+<a href="{{ asset('singgasana-sport/public/qr_codes/' . $data->qr) }}" download="{{ $data->qr }}" style="background-color: #007bff; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">
+    Unduh QR Code
+</a>
 <div class="row mb-2"><div class="col-12">Harap simpan QR ini dengan kerahasiaan dan gunakan untuk tiket Anda.</div></div>
 <div class="row mb-2"><div class="col-12">Harap diingat bahwa kode QR ini akan berlaku hingga <b>{{ date('d-m-Y H:i:s', strtotime($data->expired)) }}</b>. Setelah waktu tersebut, kode ini akan tidak berlaku dan Anda perlu melakukan proses booking kembali.</div></div>
 @else
