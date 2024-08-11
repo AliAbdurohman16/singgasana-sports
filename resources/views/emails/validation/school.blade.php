150b28db636f71c5595c96e6b76d1c3fa0068988
AliAbdurohman16 <alyabdurohman16@gmail.com>
2024-08-11 14:26:47 +0700

    comment button unduh qr

--- resources/views/emails/validation/school.blade.php
+++ resources/views/emails/validation/school.blade.php
@@ -3,10 +3,10 @@
 <h1 class="text-center mb-4">Validasi Berhasil</h1>
 <div class="row mb-2"><div class="col-12">Halo {{ $data->user->first_name }},</div></div>
 <div class="row mb-2"><div class="col-12">Kami senang memberitahu Anda bahwa proses validasi Anda baru-baru ini berhasil.</div></div>
-{{-- @component('mail::button', ['url' => asset('qr_codes/' . $data->qr), 'color' => 'primary'])
+<!-- {{-- @component('mail::button', ['url' => asset('qr_codes/' . $data->qr), 'color' => 'primary'])
 Unduh QR Code
 @endcomponent --}}
-{{-- <div class="row mb-2"><div class="col-12">Harap simpan QR ini dengan kerahasiaan dan gunakan untuk tiket Anda.</div></div>
+{{-- <div class="row mb-2"><div class="col-12">Harap simpan QR ini dengan kerahasiaan dan gunakan untuk tiket Anda.</div></div> -->
 <div class="row mb-2"><div class="col-12">Harap diingat bahwa kode QR ini akan berlaku hingga <b>{{ date('d-m-Y H:i:s', strtotime($data->expired)) }}</b>. Setelah waktu tersebut, kode ini akan tidak berlaku dan Anda perlu melakukan proses booking kembali.</div></div> --}}
 <div class="row mb-2"><div class="col-12">Jika Anda memiliki pertanyaan atau kekhawatiran, jangan ragu untuk menghubungi tim dukungan kami.</div></div>
 <div class="row mb-4"><div class="col-12">Terima kasih telah memilih layanan kami!</div></div>
