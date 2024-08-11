299b68bb15531edb5bc7c83871ca7eca8eb2bd9c
AliAbdurohman16 <alyabdurohman16@gmail.com>
2024-08-11 21:18:40 +0700

    show qr

--- resources/views/emails/validation/member.blade.php
+++ resources/views/emails/validation/member.blade.php
@@ -4,10 +4,12 @@
 <div class="row mb-2"><div class="col-12">Halo {{ $data->user->first_name }},</div></div>
 @if ($data->service->name == 'Swimming Pool')
 <div class="row mb-2"><div class="col-12">Kami senang memberitahu Anda bahwa proses validasi Anda baru-baru ini berhasil.</div></div>
-<!-- @component('mail::button', ['url' => asset('qr_codes/' . $data->qr), 'color' => 'primary'])
-Unduh QR Code
-@endcomponent
-<div class="row mb-2"><div class="col-12">Harap simpan QR ini dengan kerahasiaan dan gunakan untuk tiket Anda.</div></div> -->
+<div class="row mb-2">
+    <div class="col-12 qr-code-container" style="background-color: #ecebeb; padding: 20px;">
+        <img src="{{ asset('singgasana-sport/public/qr_codes/' . $data->qr) }}" style="display: block; margin: auto; text-align: center; width: 200px;" alt="QR Code">
+    </div>
+</div>
+<div class="row mb-2"><div class="col-12">Harap simpan QR ini dengan kerahasiaan dan gunakan untuk tiket Anda.</div></div>
 <div class="row mb-2"><div class="col-12">Harap diingat bahwa kode QR ini akan berlaku hingga <b>{{ date('d-m-Y H:i:s', strtotime($data->expired)) }}</b>. Setelah waktu tersebut, kode ini akan tidak berlaku dan Anda perlu melakukan proses booking kembali.</div></div> --}}
 @else
 <div class="row mb-2"><div class="col-12">Kami senang memberitahu Anda bahwa proses validasi Anda baru-baru ini berhasil. Berikut adalah kode PIN Anda:</div></div>
